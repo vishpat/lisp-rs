02c5ecc0bc2dfb4f55c58467eee5764f2c8432ac
Vishal Patil <vishpat@gmail.com>
2022-04-28 16:19:52 -0400

    Parsing completing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use crate::lexer::*;
 use crate::object::*;
-use std::fmt;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ParseError {
@@ -15,14 +15,14 @@ impl fmt::Display for ParseError {
 }
 
 impl Error for ParseError {}
-    
-
 
 pub fn parse_list(tokens: &mut Vec<Token>) -> Result<Object, ParseError> {
-
     let token = tokens.pop();
     if token != Some(Token::LParen) {
-        println!("Did not find LParen, found {:?}, remaining tokens {:?}", token, tokens);
+        println!(
+            "Did not find LParen, found {:?}, remaining tokens {:?}",
+            token, tokens
+        );
         return Err(ParseError {
             token: token.unwrap(),
         });
@@ -44,7 +44,7 @@ pub fn parse_list(tokens: &mut Vec<Token>) -> Result<Object, ParseError> {
             Token::Symbol(s) => list.push(Object::Symbol(s)),
             Token::LParen => {
                 tokens.push(Token::LParen);
-                let sub_list = parse_list( tokens)?;
+                let sub_list = parse_list(tokens)?;
                 list.push(sub_list);
             }
             Token::RParen => {
@@ -54,10 +54,61 @@ pub fn parse_list(tokens: &mut Vec<Token>) -> Result<Object, ParseError> {
                 println!("Unexpected token {:?}", t);
                 return Err(ParseError {
                     token: Token::Invalid,
-                })
+                });
             }
         }
     }
 
     Ok(Object::List(list))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let mut tokens = tokenize("(+ 1 2)").unwrap_or(vec![]);
+        let list = parse_list(&mut tokens.into_iter().rev().collect()).unwrap();
+        assert_eq!(
+            list,
+            Object::List(vec![
+                Object::Symbol("+".to_string()),
+                Object::Integer(1),
+                Object::Integer(2),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_area_of_a_circle() {
+        let mut tokens =
+            tokenize("(begin (define r 10)(define pi 3.14)(* pi (* r r)))").unwrap_or(vec![]);
+        let list = parse_list(&mut tokens.into_iter().rev().collect()).unwrap();
+        assert_eq!(
+            list,
+            Object::List(vec![
+                Object::Symbol("begin".to_string()),
+                Object::List(vec![
+                    Object::Symbol("define".to_string()),
+                    Object::Symbol("r".to_string()),
+                    Object::Integer(10),
+                ]),
+                Object::List(vec![
+                    Object::Symbol("define".to_string()),
+                    Object::Symbol("pi".to_string()),
+                    Object::Float(3.14),
+                ]),
+                Object::List(vec![
+                    Object::Symbol("*".to_string()),
+                    Object::Symbol("pi".to_string()),
+                    Object::List(vec![
+                        Object::Symbol("*".to_string()),
+                        Object::Symbol("r".to_string()),
+                        Object::Symbol("r".to_string()),
+                    ]),
+                ]),
+            ])
+        );
+    }
 }
