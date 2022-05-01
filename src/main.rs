34e43ad9f94e7058dc2757a731bb0ed33256cd16
Vishal Patil <vishpat@gmail.com>
2022-05-01 12:02:37 -0400

    Added interpreter

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ mod lexer;
 mod object;
 mod parser;
 
+use linefeed::{Interface, ReadResult};
 use std::cell::RefCell;
 use std::rc::Rc;
 
+const PROMPT: &str = "lisp-rs> ";
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let reader = Interface::new(PROMPT).unwrap();
     let mut env = Rc::new(RefCell::new(env::Env::new()));
-    let val = eval::eval("(+ 2 3)", &mut env)?;
-    println!("{:?}", val);
-    Ok(())
+
+    reader.set_prompt(format!("{}", PROMPT).as_ref()).unwrap();
+
+    while let ReadResult::Input(input) = reader.read_line().unwrap() {
+        if input.eq("exit") {
+            break;
+        }
+        println!("{:?}", eval::eval(input.as_ref(), &mut env));
+    }
+
+    println!("Good bye");
+    Ok(()) 
 }
