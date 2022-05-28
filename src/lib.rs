b3cc97382c2e662306b117b7687a2f01d02054fd
Vishal Patil <vishpat@gmail.com>
2022-05-28 16:06:30 -0400

    WASM

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+mod env;
+mod eval;
+mod lexer;
+mod object;
+mod parser;
+
+use object::Object;
+use std::cell::RefCell;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn eval(input: String) -> String {
+    let mut env = Rc::new(RefCell::new(env::Env::new()));
+    let val = eval::eval(input.as_ref(), &mut env);
+    match val {
+        Ok(Object::Void) => "".to_string(),
+        Ok(Object::Integer(n)) => n.to_string(),
+        Ok(Object::Bool(b)) => b.to_string(),
+        Ok(Object::Symbol(s)) => s.to_string(),
+        Ok(Object::Lambda(params, body, _)) => {
+            let mut res = "Lambda(".to_string();
+            for param in params {
+                res.push_str(&format!("{} ", param));
+            }
+            res.push_str(")");
+            for expr in (*body).iter() {
+                res.push_str(&format!(" {}", expr));
+            }
+            res
+        }
+        Ok(Object::List(list)) => {
+            let mut res = "(".to_string();
+            for (i, obj) in (*list).iter().enumerate() {
+                if i > 0 {
+                    res.push_str(" ");
+                }
+                res.push_str(&format!("{}", obj));
+            }
+            res.push_str(")");
+            res
+        }
+        Ok(Object::ListData(list)) => {
+            let mut res = "(".to_string();
+            for (i, obj) in list.iter().enumerate() {
+                if i > 0 {
+                    res.push_str(" ");
+                }
+                res.push_str(&format!("{}", obj));
+            }
+            res.push_str(")");
+            res
+        }
+        Ok(Object::String(s)) => s.to_string(),
+        Ok(Object::Keyword(s)) => s.to_string(),
+        Ok(Object::BinaryOp(s)) => s.to_string(),
+        Ok(Object::Float(n)) => n.to_string(),
+        Ok(Object::If) => "If".to_string(),
+        Err(e) => e.to_string(),
+    }
+}
